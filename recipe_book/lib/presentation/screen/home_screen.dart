76ecef8e4efe4cff04fd73cded59886e47669489
sakifmishad <109865291+mishad01@users.noreply.github.com>
2026-04-04 23:29:34 +0600

    feat: enhance HomeScreen with category selection and recipe fetching; add RecipeCard widget for displaying recipes

--- recipe_book/lib/presentation/screen/home_screen.dart
+++ recipe_book/lib/presentation/screen/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:recipe_book/core/app_colors.dart';
 import 'package:recipe_book/core/app_strings.dart';
+import 'package:recipe_book/presentation/provider/recipe_provider.dart';
 import 'package:recipe_book/presentation/screen/search_screen.dart';
+import 'package:recipe_book/presentation/widget/recipe_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,12 +25,13 @@ class _HomeScreenState extends State<HomeScreen> {
     'Japanese',
   ];
 
-  final String _selectedCategory = 'All';
+  String _selectedCategory = 'All';
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    context.read<RecipeProvider>().fetchRecipesByCategory(_selectedCategory);
   }
 
   @override
@@ -36,7 +40,10 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: AppColors.surface,
       appBar: AppBar(
         backgroundColor: AppColors.surface,
-        title: Column(children: [Text('Recipe Book'), Text('Recipe Book')]),
+        title: Column(
+          crossAxisAlignment: .start,
+          children: [Text(' Book'), Text('Recipe Book')],
+        ),
         centerTitle: false,
         leading: Padding(
           padding: const EdgeInsets.all(6.0),
@@ -61,6 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            spacing: 10,
             crossAxisAlignment: .start,
             children: [
               Text(
@@ -68,6 +76,28 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
               ),
               _buildCategoryCard(),
+              Expanded(
+                child: Consumer<RecipeProvider>(
+                  builder: (context, provider, child) {
+                    if (provider.isLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (provider.categoryRecipes.isEmpty) {
+                      return const Center(child: Text('No recipes found'));
+                    }
+                    return ListView.builder(
+                      scrollDirection: .horizontal,
+                      clipBehavior: .none,
+                      itemCount: provider.categoryRecipes.length,
+                      itemBuilder: (context, index) {
+                        return RecipeCard(
+                          recipe: provider.categoryRecipes[index],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -76,25 +106,48 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildCategoryCard() {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        final category = categories[index];
-        final isSelected = category == _selectedCategory;
-        return Container(
-          height: 100,
-          padding: .symmetric(horizontal: 16, vertical: 8),
-          child: Text(
-            category,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-              color: isSelected ? AppColors.primary : AppColors.grey,
+    return SizedBox(
+      height: 40,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        clipBehavior: Clip.none,
+        itemBuilder: (context, index) {
+          final category = categories[index];
+          final isSelected = category == _selectedCategory;
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedCategory = category;
+                context.read<RecipeProvider>().fetchRecipesByCategory(
+                  _selectedCategory,
+                );
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Container(
+                padding: .symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  color: isSelected ? AppColors.primary : AppColors.surface,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: isSelected ? AppColors.primary : AppColors.grey,
+                  ),
+                ),
+                child: Text(
+                  category,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: isSelected ? AppColors.white70 : AppColors.grey,
+                  ),
+                ),
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
--- recipe_book/lib/presentation/widget/recipe_card.dart
+++ recipe_book/lib/presentation/widget/recipe_card.dart
@@ -1,1 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_book/core/app_colors.dart';
+import 'package:recipe_book/domain/entities/recipe.dart';
 
+class RecipeCard extends StatelessWidget {
+  const RecipeCard({super.key, required this.recipe});
+  final Recipe recipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: 180,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(20),
+                  child: CachedNetworkImage(imageUrl: recipe.image),
+                ),
+                Positioned(
+                  left: 12,
+                  top: 12,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 6,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.customOverlay,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisSize: .min,
+                      children: [
+                        Icon(Icons.star, color: Colors.yellow, size: 14),
+                        Text('4.5', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 170,
+              child: Text(
+                recipe.title,
+                style: TextStyle(fontWeight: .w800, fontSize: 16),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: .center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
