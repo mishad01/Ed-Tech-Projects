8a21ebb2dda426dbe2cdd4152b46ecd5ed0273ef
sakifmishad <109865291+mishad01@users.noreply.github.com>
2026-04-05 23:59:08 +0600

    feat: add null check and empty state UI to result screen and update provider access to watch

--- lottery_app/lib/presentation/screen/home_screen.dart
+++ lottery_app/lib/presentation/screen/home_screen.dart
@@ -90,6 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 ElevatedButton(
                   onPressed: () {
+                    provider.playLottery();
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => ResultScreen()),
--- lottery_app/lib/presentation/screen/result_screen.dart
+++ lottery_app/lib/presentation/screen/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottery_app/core/app_colors.dart';
 import 'package:lottery_app/core/app_strings.dart';
 import 'package:lottery_app/presentation/provider/lottery_provider.dart';
 import 'package:lottery_app/presentation/widget/gradient_scaffold.dart';
@@ -16,18 +17,31 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<LotteryProvider>(context);
+    final provider = context.watch<LotteryProvider>();
     final result = provider.getLotteryResult;
+
+    if (result == null) {
+      return GradientScaffold(
+        showBackButton: true,
+        title: AppStrings.resultTitle,
+        body: SafeArea(child: Center(child: Text(AppStrings.noResultFound))),
+      );
+    }
+
     return GradientScaffold(
       showBackButton: true,
+      gradientColors: result.isWinner ? AppColors.winGradient : null,
       title: AppStrings.resultTitle,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              ResultIcon(isWinner: result?.isWinner ?? false),
-              ResultNumberCard(result: result!),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              children: [
+                ResultIcon(isWinner: result.isWinner),
+                ResultNumberCard(result: result),
+              ],
+            ),
           ),
         ),
       ),
